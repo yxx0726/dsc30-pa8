fb3cd03dd1607e9b7fb3cc4d919ebf4d8a607556
yxx0726 <49292731+yxx0726@users.noreply.github.com>
2019-11-26 23:24:37 -0800

    8.9

--- src/MyCompressor.java
+++ src/MyCompressor.java
@@ -22,23 +22,23 @@ public class MyCompressor {
         //todo
         //run compression and decompression here
         MyCompressor temp = new MyCompressor(args[0]);
-        if(args[1].equals("compress")){
+        if (args[1].equals("compress")) {
             //if compress, target the name and compress.
-            for(int i = 2; i < args.length;i++){
+            for(int i = 2; i < args.length; i++){
                 File f = new File(args[i]);
-                String path1 = f.getPath().substring(0,f.getPath().length()-f.getName().length());
-                String name2 = "compressed_"+f.getName();
+                String path1 = f.getPath().substring(0,f.getPath().length() - f.getName().length());
+                String name2 = "compressed_" + f.getName();
                 temp.compress(args[i],path1+name2);
             }
         }
         //if decompress.
-        if(args[1].equals("decompress")){
+        if (args[1].equals("decompress")) {
             //get the target name and ready to decompress.
-            for(int i = 2; i < args.length;i++){
+            for (int i = 2; i < args.length; i++) {
                 File f = new File(args[i]);
-                String path1 = f.getPath().substring(0,f.getPath().length()-f.getName().length());
-                String name2 = "decompressed_"+f.getName();
-                temp.decompress(args[i],path1+name2);
+                String path1 = f.getPath().substring(0, f.getPath().length() - f.getName().length());
+                String name2 = "decompressed_" + f.getName();
+                temp.decompress(args[i],path1 + name2);
             }
         }
 
@@ -62,14 +62,14 @@ public class MyCompressor {
      * @param corpusLocation directory to corpus text file
      * @return reference to the built HCTree
      */
-    private HCTree buildHuffmanTree(String corpusLocation) throws IOException{
+    private HCTree buildHuffmanTree(String corpusLocation) throws IOException {
 
         //todo
         byte[] in = Files.readAllBytes(Paths.get(corpusLocation));
         int[] freqAry = new int[NUM_CHAR];
         HCTree cp = new HCTree();
         for (int i = 0; i < in.length; i++) {
-            freqAry[in[i] & 0XFF] ++;
+            freqAry[in[i] & 0XFF]++;
         }
         cp.buildTree(freqAry);
         return cp;
@@ -86,10 +86,10 @@ public class MyCompressor {
         FileOutputStream comp = new FileOutputStream(compressedFile);
         DataOutputStream out  = new DataOutputStream(comp);
         BitOutputStream bOut = new BitOutputStream(out);
-        byte[] input = Files.readAllBytes( Paths.get(inputFile));
+        byte[] input = Files.readAllBytes(Paths.get(inputFile));
         out.writeInt(input.length);
-        for(int i = 0; i < input.length;i++){
-            HuffmanTree.encode(input[i],bOut);
+        for (int i = 0; i < input.length; i++) {
+            HuffmanTree.encode(input[i], bOut);
         }
         bOut.flush();
         out.close();
@@ -102,7 +102,7 @@ public class MyCompressor {
      * @param outputFile directory of uncompressed file.
      */
     public void decompress(String compressedFile, String outputFile) throws IOException {
-        
+
         FileInputStream inFile = new FileInputStream(compressedFile);
         DataInputStream in = new DataInputStream(inFile);
         BitInputStream bitIn = new BitInputStream(in);
@@ -110,7 +110,7 @@ public class MyCompressor {
         DataOutputStream out = new DataOutputStream(outFile);
 
         int len = in.readInt();
-        for(int i = 0; i < len;i++){
+        for (int i = 0; i < len; i++) {
             out.writeByte(HuffmanTree.decode(bitIn));
         }
         inFile.close();
